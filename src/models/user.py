792449a5ddab9e970d09e8b0aceadf092cf77043
Zbigniew Siciarz <zbigniew@siciarz.net>
2022-04-16 20:43:03 +0200

    Add repr for User model.

--- src/models/user.py
+++ src/models/user.py
@@ -10,3 +10,6 @@ class User(Base):
     id: M[int] = Column(Integer, primary_key=True)
     email: M[str] = Column(String, unique=True, nullable=False)
     name: M[str | None] = Column(String, nullable=True)
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, email={self.email!r})"
