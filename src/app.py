8e4c0c464c0d7359837eb4e379e8c777ed98dd36
Zbigniew Siciarz <zbigniew@siciarz.net>
2022-04-17 17:24:46 +0200

    Move custom ASGI app to graphql_api.asgi module.

--- src/app.py
+++ src/app.py
@@ -1,35 +1,11 @@
 import asyncio
-from typing import Optional, Union
 
-from starlette.requests import Request
-from starlette.responses import Response
-from starlette.websockets import WebSocket
-from strawberry.asgi import GraphQL
-
+from graphql_api.asgi import JamchemyGraphQL
 from graphql_api.schema import schema
-from graphql_api.types import Context
-from models.base import Session
 
 # mutation puts User IDs on queue, subscription consumes the ID
 # note: this assumes running in a single process
 queue: asyncio.Queue[int] = asyncio.Queue()
 
 
-class JamchemyGraphQL(GraphQL):
-    async def get_context(
-        self,
-        request: Union[Request, WebSocket],
-        response: Optional[Response] = None,
-    ) -> Context:
-        async with Session() as session:
-            # TODO: Should we call session.begin() here? Should the entire
-            # request be atomic with regard to database transactions?
-            return {
-                "queue": queue,
-                "request": request,
-                "response": response,
-                "session": session,
-            }
-
-
-app = JamchemyGraphQL(schema)
+app = JamchemyGraphQL(schema, queue=queue)
---
+++ src/graphql_api/asgi.py
@@ -0,0 +1,32 @@
+import asyncio
+from typing import Any, Optional, Union
+
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.websockets import WebSocket
+from strawberry.asgi import GraphQL
+from strawberry.schema import BaseSchema
+
+from graphql_api.types import Context
+from models.base import Session
+
+
+class JamchemyGraphQL(GraphQL):
+    def __init__(self, schema: BaseSchema, queue: asyncio.Queue[int], **kwargs: Any):
+        super().__init__(schema, **kwargs)
+        self.queue = queue
+
+    async def get_context(
+        self,
+        request: Union[Request, WebSocket],
+        response: Optional[Response] = None,
+    ) -> Context:
+        async with Session() as session:
+            # TODO: Should we call session.begin() here? Should the entire
+            # request be atomic with regard to database transactions?
+            return {
+                "queue": self.queue,
+                "request": request,
+                "response": response,
+                "session": session,
+            }
