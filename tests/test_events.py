47bc04b1a143f6085d9a5ced96662bdbbe04bceb
Zbigniew Siciarz <zbigniew@siciarz.net>
2022-04-18 15:13:21 +0200

    Don't use testcase-like name in tests.

--- tests/test_events.py
+++ tests/test_events.py
@@ -7,21 +7,21 @@ from events import Event, EventManager, Subscriber
 
 
 @dataclasses.dataclass(frozen=True)
-class TestEvent(Event):
+class ExampleEvent(Event):
     message: str
 
 
 @pytest.mark.asyncio
 async def test_simple_pubsub() -> None:
     event_manager = EventManager()
-    subscriber = await event_manager.subscribe(TestEvent)
+    subscriber = await event_manager.subscribe(ExampleEvent)
 
-    async def consume_event(s: Subscriber[TestEvent]) -> str:
+    async def consume_event(s: Subscriber[ExampleEvent]) -> str:
         event = await anext(s.events())
         return event.message
 
-    test_event = TestEvent(message="Hello world")
-    producer = asyncio.create_task(event_manager.publish(test_event))
+    example_event = ExampleEvent(message="Hello world")
+    producer = asyncio.create_task(event_manager.publish(example_event))
     consumer = asyncio.create_task(consume_event(subscriber))
     _, message = await asyncio.gather(producer, consumer)
-    assert message == test_event.message
+    assert message == example_event.message
