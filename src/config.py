85628e7f95a231a66acaa0baf2aa93f700fc8e35
Zbigniew Siciarz <zbigniew@siciarz.net>
2022-04-19 19:25:09 +0200

    Configure stdlib logging to format through structlog.

--- src/config.py
+++ src/config.py
@@ -2,16 +2,29 @@ import logging.config
 import os
 
 import structlog
+from structlog.types import Processor
 
 DATABASE_URI = os.environ.get(
     "DATABASE_URI",
     "sqlite+aiosqlite:///jamchemy.db",
 )
 
+pre_chain: list[Processor] = [
+    structlog.stdlib.add_log_level,
+    structlog.stdlib.add_logger_name,
+    structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S"),
+]
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": True,
-    "formatters": {"structlog": {"format": "%(asctime)s %(message)s"}},
+    "formatters": {
+        "structlog": {
+            "()": structlog.stdlib.ProcessorFormatter,
+            "processor": structlog.dev.ConsoleRenderer(),
+            "foreign_pre_chain": pre_chain,
+        }
+    },
     "handlers": {
         "console": {
             "level": "DEBUG",
@@ -21,21 +34,20 @@ LOGGING = {
     },
     "loggers": {
         "events": {"handlers": ["console"], "level": "INFO"},
-        "uvicorn": {"handlers": ["console"], "level": "INFO"},
+        "uvicorn": {"handlers": ["console"], "level": "WARNING"},
     },
 }
 logging.config.dictConfig(LOGGING)
 
 structlog.configure(
-    processors=[
+    processors=pre_chain
+    + [
         structlog.stdlib.filter_by_level,
-        structlog.stdlib.add_logger_name,
-        structlog.stdlib.add_log_level,
         structlog.stdlib.PositionalArgumentsFormatter(),
         structlog.processors.StackInfoRenderer(),
         structlog.dev.set_exc_info,
         structlog.processors.format_exc_info,
-        structlog.dev.ConsoleRenderer(),
+        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
     ],
     wrapper_class=structlog.stdlib.BoundLogger,
     context_class=dict,
